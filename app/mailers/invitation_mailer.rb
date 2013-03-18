6f9ef32dfb3dae8d33671d6bff0307f250a44b3a
Jeremy Crosen <jeremy.crosen@gmail.com>
2013-03-18 00:55:54 -0700

    Updated the mailer to include invitation details. Might be done for now...

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,8 +1,9 @@
 class InvitationMailer < ActionMailer::Base
-  default from: ENV["MAILER_SENDER"] || "admin@crosen-culver-wedding.mailgun.org"
+  default from: ENV["MAILER_SENDER"] || "admin@culver-crosen-wedding.mailgun.org"
 
   def invitation_email(user)
     @user = user
+    @invitations = @user.invitations
     mail(to: user.email, subject: "Invitation to the Wedding of Monica Culver and Jeremy Crosen")
   end
 end
