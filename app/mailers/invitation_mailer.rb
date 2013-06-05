00217ca5badb8f1d47bd616f464394e269173256
Jeremy Crosen <jeremy.crosen@gmail.com>
2013-06-04 23:12:35 -0700

    Stupid postgres incompatibility

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -42,7 +42,7 @@ class InvitationMailer < ActionMailer::Base
     @user = user
     # implicit join criteria here, a little too magic...
     @attending_events = @user.events.where(invitations: {status: 'attending'}).all
-    @not_attending_events = @user.events.where('invitations.status <> "attending"').all
+    @not_attending_events = @user.events.where("invitations.status <> 'attending'").all
     
     mail(to: @user.email, subject: "Your itinerary for the Monica Culver & Jeremy Crosen Wedding", from: "Monica Culver & Jeremy Crosen <admin@culver-crosen-wedding.mailgun.org>")
   end
